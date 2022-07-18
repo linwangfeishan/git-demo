03e941bfba4fd6410b55fcf66053299d80ee2f45
dragon <1041355269@qq.com>
2022-07-18 10:22:44 +0800

    hot-fix first commit

--- src/main/java/edu/hqu/Git.java
+++ src/main/java/edu/hqu/Git.java
@@ -4,5 +4,6 @@ public class Git {
     public static void main(String[] args) {
         System.out.println("git first");
         System.out.println("git second");
+        System.out.println("git third");
     }
 }
